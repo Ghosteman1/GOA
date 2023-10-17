6d188740dce649d389dacb3decc7334802b5dc34
DemetreKharatishvili1 <143964726+Ghosteman1@users.noreply.github.com>
2023-10-17 15:31:50 +0400

    Delete day 6 directory

--- day 6/classwork.py
+++
@@ -1,6 +0,0 @@
-#classwork is: print numbers that are multiples of three.
-
-num = 0  #variebles 
-while num <=30:  
-    print(str(num) + " numbers that are multiples of three ")
-    num = num + 3
