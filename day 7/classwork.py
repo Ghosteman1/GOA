7ec09c8b48f997b2b38376d3e6c61b9ec22bb2c8
DemetreKharatishvili1 <143964726+Ghosteman1@users.noreply.github.com>
2023-10-17 18:25:37 +0400

    <3

---
+++ day 7/classwork.py
@@ -0,0 +1,4 @@
+number = int(input("enter number: "))
+while number >=0:
+    print(number)
+    number -= 1
---
+++ day 7/classwork1.py
@@ -0,0 +1,9 @@
+#for i in range (3):
+    #print(i<1)
+
+
+password = "SecretWord"
+guess = input("Enter: ")
+while guess != password:
+    guess = input("enter: ")
+print("Access Granted!")
